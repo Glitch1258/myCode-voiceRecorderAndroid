f0a94e174a2745ca8e1ef5011bde5955fb5e6d69
Glitch12358 <glitch12358@gmail.com>
2024-11-16 19:06:59 +0600

    fifth

---
+++ src/java/com/myPackage/voiceRecorder/main.java
@@ -0,0 +1,10 @@
+package com.myPackage.voicerecorder;
+
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
